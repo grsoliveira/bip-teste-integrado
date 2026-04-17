e7257f3917c28441e136cf59e5cad983321f4f25
Gleibson Oliveira <gleibson.rodrigo>
2026-04-17 01:27:46 -0300

    config: usando entrypoint no Dockerfile e removendo o modo interativo -it do docker run.

--- ejb-module/run.sh
+++ ejb-module/run.sh
@@ -10,4 +10,4 @@ echo "Buildando imagem Docker..."
 docker build -t ejb-app . || exit 1
 
 echo "Subindo container (em modo interativo)..."
-docker run -it -p 8083:8080 ejb-app
+docker run -p 8083:8080 -p 9993:9990 ejb-app
