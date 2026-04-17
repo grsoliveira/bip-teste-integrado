3c3c88768b25990fa75bf6285b9d686d26f715f9
Gleibson Oliveira <gleibson.rodrigo>
2026-04-16 22:07:18 -0300

    config: inclusao de findAll e init (com postconstruct para debug). modificação temporária das Annotations da classe para teste. Vai ser revisado na integraçõa.

--- ejb-module/src/main/java/com/example/ejb/BeneficioEjbService.java
+++ ejb-module/src/main/java/com/example/ejb/BeneficioEjbService.java
@@ -1,25 +1,47 @@
 package com.example.ejb;
 
-import jakarta.ejb.Stateless;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.logging.Logger;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import java.math.BigDecimal;
 
-@Stateless
+//TODO revisar necessidade de todas essas tags quando a integração foi feita com o backend.
+@Singleton
+@Startup
+@LocalBean
 public class BeneficioEjbService {
 
-    @PersistenceContext(unitName = "bipPU")
-    private EntityManager em;
+  @PersistenceContext(unitName = "bipPU")
+  private EntityManager em;
 
-    public void transfer(Long fromId, Long toId, BigDecimal amount) {
-        Beneficio from = em.find(Beneficio.class, fromId);
-        Beneficio to   = em.find(Beneficio.class, toId);
+  private static final Logger log = Logger.getLogger(BeneficioEjbService.class.getName());
 
-        // BUG: sem validações, sem locking, pode gerar saldo negativo e lost update
-        from.setValor(from.getValor().subtract(amount));
-        to.setValor(to.getValor().add(amount));
+  public void transfer(Long fromId, Long toId, BigDecimal amount) {
+    Beneficio from = em.find(Beneficio.class, fromId);
+    Beneficio to = em.find(Beneficio.class, toId);
 
-        em.merge(from);
-        em.merge(to);
-    }
+    // BUG: sem validações, sem locking, pode gerar saldo negativo e lost update
+    from.setValor(from.getValor().subtract(amount));
+    to.setValor(to.getValor().add(amount));
+
+    em.merge(from);
+    em.merge(to);
+  }
+
+  public List<Beneficio> findAll() {
+    return em.createQuery("SELECT b FROM Beneficio b", Beneficio.class)
+        .getResultList();
+  }
+
+  @PostConstruct
+  public void init() {
+    log.info("### TESTANDO EJB ###");
+    this.findAll().forEach(b -> log.info(b.getNome()));
+  }
 }
