c78992215635d247b0b92cec0d4129e9383059bf
Gleibson Oliveira <gleibson.rodrigo>
2026-04-16 21:24:22 -0300

    config: modificação da classe Beneficio, junção dos scripts no import.sql e ajuste para o persistence.xml criar o banco

--- ejb-module/src/main/java/com/example/ejb/Beneficio.java
+++ ejb-module/src/main/java/com/example/ejb/Beneficio.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +19,28 @@ public class Beneficio {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID")
   private Long id;
 
   @Setter
-  @Column(nullable = false)
+  @Column(name = "NOME", length = 100)
+  private String nome;
+
+  @Setter
+  @Column(name = "DESCRICAO")
+  private String descricao;
+
+  @Setter
+  @Column(name = "ATIVO", nullable = false, columnDefinition = "BOOLEAN DEFAULT TRUE")
+  private Boolean ativo = true;
+
+  //TODO revisar - já inserindo flag para o optimistic locking
+  @Version
+  @Column(name = "VERSION")
+  private Long version;
+
+  @Setter
+  @Column(name = "VALOR", nullable = false, columnDefinition = "DECIMAL(15, 2)")
   private BigDecimal valor;
 
 }
