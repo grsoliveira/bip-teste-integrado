97830881cd44f07f012d67d98c4ba07852b734ea
Gleibson Oliveira <gleibson.rodrigo>
2026-04-19 15:40:44 -0300

    config: centralizar informação do api path, usando api-config corretamente.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BENEFICIO_PROVIDERS } from './domains/beneficio/beneficio.providers';
+import { API_BASE_URL } from './core/api.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    { provide: API_BASE_URL, useValue: 'http://localhost:8080/api/v1' },
     ...BENEFICIO_PROVIDERS,
   ],
 };
